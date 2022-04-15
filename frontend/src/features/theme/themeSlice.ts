6863eb7a354826916a81e911b74fe6ec75d267d7
Vaclav Honzik <vaclavhonzik98@gmail.com>
2022-04-15 15:13:04 +0200

    dark mode

---
+++ frontend/src/features/theme/themeSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { Theme } from '@mui/material/styles'
+import { PaletteMode } from '@mui/material'
+
+export interface ThemeState {
+    paletteMode: PaletteMode
+}
+
+const persistConfig = {
+    key: 'theme',
+    storage, // localStorage for browsers
+}
+
+const initialState: ThemeState = {
+    paletteMode: 'light',
+}
+
+const themeSlice = createSlice({
+    name: 'theme',
+    initialState,
+    reducers: {
+        toggleTheme: (state) => ({
+            ...state,
+            paletteMode: state.paletteMode === 'light' ? 'dark' : 'light',
+        }),
+    },
+})
+
+const themeReducer = persistReducer(persistConfig, themeSlice.reducer)
+// const themeReducer = themeSlice.reducer
+export const { toggleTheme } = themeSlice.actions
+export default themeReducer
